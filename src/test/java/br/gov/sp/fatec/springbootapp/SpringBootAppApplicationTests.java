63b1748d761ef02c11d8cd6b22c9dc09189dbb48
AAMergulhao <alves.m.arthur@gmail.com>
2020-09-20 19:11:42 +0000

    Aula 2

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,39 @@
 package br.gov.sp.fatec.springbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional // Cria transação para cada teste
+@Rollback // não comita as transações
 class SpringBootAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @Test
+    void testeInsercao(){
+        Usuario usuario = new Usuario();
+
+        usuario.setNome("TESTE");
+        usuario.setSenha("senha");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
+    }
+
+    @Test
+    void testeAutorizacao(){
+        Usuario usuario = usuarioRepo.findById(1L).get();
+
+        assertEquals("ROLE_ADMIN", usuario.getAutorizacoes().iterator().next().getNome());
+    }
 
 }
